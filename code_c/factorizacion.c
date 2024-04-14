642f977c2cc056cd2a2f305894c27465a3d345de
adpl18 <adominguez18>
2024-04-14 19:00:52 -0400

    grafico

---
+++ code_c/factorizacion.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// gcc factorizacion_primo.c -o factorizacion_primo
+// ./factorizacion_primo
+
+bool esPrimo(long long int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+char* factorizacionPrima(const char* num) {
+    // Parsea el char a un long long int
+    long long int n = atoll(num);
+    // long long int n = strtoll(char_num, NULL, 10);
+    long long int divisor = 2;
+    char* resultado = (char*)malloc(1000 * sizeof(char));
+
+    while (n > 1) {
+        if (n % divisor == 0) {
+            if (esPrimo(divisor)) {
+                char temp[100];
+                sprintf(temp, "%lld ", divisor);
+                strcat(resultado, temp);
+            }
+            n /= divisor;
+        } else {
+            divisor++;
+        }
+    }
+  return resultado;
+}
+
+char* factorizacionPrima2(const char* num) {
+    // Parsea el char a un long long int
+    long long int n = atoll(num);
+    // long long int n = strtoll(char_num, NULL, 10);
+    long long int divisor = 2;
+    char* resultado = (char*)malloc(1000 * sizeof(char));
+
+    while (n > 1) {
+        if (n % divisor == 0) {
+            if (esPrimo(divisor)) {
+                char temp[100];
+                sprintf(temp, "%lld ", divisor);
+                strcat(resultado, temp);
+            }
+            n /= divisor;
+        } else {
+            divisor++;
+        }
+    }
+  return resultado;
+}
+
+char* factorizacionPrima3(const char* num) {
+    // Parsea el char a un long long int
+    long long int n = atoll(num);
+    // long long int n = strtoll(char_num, NULL, 10);
+    long long int divisor = 2;
+    char* resultado = (char*)malloc(1000 * sizeof(char));
+
+    while (n > 1) {
+        if (n % divisor == 0) {
+            if (esPrimo(divisor)) {
+                char temp[100];
+                sprintf(temp, "%lld ", divisor);
+                strcat(resultado, temp);
+            }
+            n /= divisor;
+        } else {
+            divisor++;
+        }
+    }
+  return resultado;
+}
